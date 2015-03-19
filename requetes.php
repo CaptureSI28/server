7917d2bf94da52e81731d6be3af7bbcaac5abbfb
BaptisteAmato <baptiste.amato@gmail.com>
2015-03-19 20:59:36 +0100

    Preparation des requetes

--- requetes.php
+++ requetes.php
@@ -2,21 +2,34 @@
 
 //insertion nouvelle partie dans la table PARTIE
 
+if ($stmt = $connect->prepare("INSERT INTO partie VALUES ('".$id_partie."','".$date_debut."','".$date_fin."')")) 
+{
+    $stmt->execute();
+    $stmt->close();
+}
 
+//insertion nouveau joueur dans la table EQUIPE
 
+if ($stmt = $connect->prepare("INSERT INTO equipe VALUES (NULL,'".$partie."','".$equipe."')"))            //NULL --> id_partie en AUTO_INCREMENT
+{
+    $stmt->execute();
+    $stmt->close();
+}
 
 //insertion nouveau joueur dans la table JOUEURS
 
-
-
-
+if ($stmt = $connect->prepare("INSERT INTO joueur VALUES ('".$login_joueur."','".$equipe."')")) 
+{
+    $stmt->execute();
+    $stmt->close();
+}
 
 //insertion données dans la table FLASHER
 
-$query = "INSERT INTO flasher VALUES ('".$date."','".$login_joueur."','".$qrcode."')";
-echo "<br>$query<br>";
-$result=mysqli_query($connect, $query);
-if (!$result) 
-	{echo "<br>pas bon : ".mysqli_error($connect);}
+if ($stmt = $connect->prepare("INSERT INTO flasher VALUES (NOW(),'".$login_joueur."','".$qrcode."')")) 
+{
+    $stmt->execute();
+    $stmt->close();
+}
 
 ?>
