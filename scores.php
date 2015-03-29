fcbd60e22fdf7d12798d5a4a126ba1cc4571723a
Thomas Riccioli <thomas.riccioli@etu.utc.fr>
2015-03-29 19:56:09 +0200

    Etat de capture des qrcodes

--- scores.php
+++ scores.php
@@ -1,23 +1,62 @@
 <?php
-	$dbconn = pg_connect('host=' . DB_HOST . ' dbname=' . DB_NAME . ' user=' . DB_USER . ' password=' . DB_PASSWORD);
-	$res = pg_prepare('', "
-		SELECT id_qrcode, (
-			SELECT ce.couleur
-			FROM flashes f, joueurs j, equipes e, couleurs_equipes ce
-			WHERE q.id_qrcode = f.qrcode AND
-				f.joueur = j.id_joueur AND
-				j.equipe = e.id_equipe AND
-				e.couleur_equipe = ce.id_couleur_equipe
-			ORDER BY date_flash DESC
-			LIMIT 1
-		) AS couleur
-		FROM qrcodes q
-		GROUP BY id_qrcode;
-	");
-	$res = pg_execute('', array());
+	require_once('db_connect.php');
+?>
+	<style type="text/css">
+<?php
+		$req = $bdd->query('
+			SELECT *
+			FROM equipes;
+		');
+	while ($row = $req->fetch()) {
+?>
+		.color<?php echo $row['id_equipe']; ?> {
+			background-color: <?php echo $row['hexcolor']; ?>;
+			padding: 10px;
+		}
+<?php
+	}
+?>
+	</style>
+<?php
+	try {
+		$req = $bdd->prepare('
+			SELECT id_qrcode, equipe, (
+				SELECT hexcolor
+				FROM equipes e
+				WHERE q.equipe = e.id_equipe
+			) AS hexcolor
+			FROM (
+				SELECT id_qrcode, (
+					SELECT (
+						SELECT equipe
+						FROM inscriptions i
+						WHERE j.id_joueur = i.joueur
+							AND i.date_inscription < date_flash
+						ORDER BY date_inscription
+						LIMIT 1
+					) AS equipe
+					FROM flashs f, joueurs j
+					WHERE q.id_qrcode = f.qrcode
+						AND f.joueur = j.id_joueur
+					ORDER BY date_flash DESC
+					LIMIT 1
+				) AS equipe
+				FROM qrcodes q
+			) q;
+		');
+		$req->execute(array());
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+	echo '<p>État des captures des qrcodes:</p>';
 	echo '<ul>';
-	while ($row = pg_fetch_array($res)) {
-		echo '<li>QRCode ' . $row['id_qrcode'] . ' capture par l\'equipe "' . $row['couleur'] . '"</li>';
+	while ($row = $req->fetch()) {
+		if ($row['equipe']) {
+			echo '<li class="color' . $row['equipe'] . '">Le QRCode ' . $row['id_qrcode'] . ' a été capturé par l\'équipe "' . $row['equipe'] . '"</li>';
+		} else {
+			echo '<li>QRCode ' . $row['id_qrcode'] . ' n\'a pas encore été capturé</li>';
+		}
 	}
 	echo '</ul>';
+	$req->closeCursor();
 ?>
