cca9b501133407eaf872b4ae6483a716cf21ac28
Thomas Riccioli <thomas.riccioli@etu.utc.fr>
2015-03-29 18:54:09 +0200

    Database structure fix

--- db_structure.sql
+++ db_structure.sql
@@ -1,76 +1,57 @@
-CREATE TABLE IF NOT EXISTS partie (
-  id_partie int NOT NULL AUTO_INCREMENT,
-  date_debut date NOT NULL,
-  date_fin date NOT NULL,
-  PRIMARY KEY (id_partie)
+CREATE TABLE IF NOT EXISTS parties (
+	id_partie INT NOT NULL AUTO_INCREMENT,
+	date_debut DATETIME NOT NULL,
+	date_fin DATETIME NOT NULL,
+	PRIMARY KEY (id_partie)
 );
 
-CREATE TABLE IF NOT EXISTS equipe (
-  id_equipe int NOT NULL,
-  PRIMARY KEY (id_equipe)
+CREATE TABLE IF NOT EXISTS equipes (
+	id_equipe INT NOT NULL AUTO_INCREMENT,
+	hexcolor VARCHAR(10),
+	PRIMARY KEY (id_equipe)
 );
 
-CREATE TABLE IF NOT EXISTS joueur (
-  login_joueur varchar(20) NOT NULL,
-  equipe int NOT NULL,
-  FOREIGN KEY (equipe) REFERENCES equipe(id_equipe),
-  PRIMARY KEY (login_joueur)
+CREATE TABLE IF NOT EXISTS joueurs (
+	id_joueur INT NOT NULL AUTO_INCREMENT,
+	login VARCHAR(20) NOT NULL UNIQUE,
+	PRIMARY KEY (id_joueur)
 );
 
-CREATE TABLE IF NOT EXISTS zone (
-  id_zone int NOT NULL,
-  PRIMARY KEY (id_zone)
+CREATE TABLE IF NOT EXISTS zones (
+	id_zone INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id_zone)
 );
 
-CREATE TABLE IF NOT EXISTS qrcode (
-  id_qrcode int NOT NULL,
-  zone int NOT NULL,
-  FOREIGN KEY (zone) REFERENCES zone(id_zone),
-  PRIMARY KEY (id_qrcode)
+CREATE TABLE IF NOT EXISTS qrcodes (
+	id_qrcode INT NOT NULL AUTO_INCREMENT,
+	zone INT NOT NULL,
+	FOREIGN KEY (zone) REFERENCES zones (id_zone),
+	PRIMARY KEY (id_qrcode)
+);
+
+CREATE TABLE IF NOT EXISTS inscriptions (
+	id_inscription INT NOT NULL AUTO_INCREMENT,
+	date_inscription DATETIME NOT NULL,
+	partie INT NOT NULL,
+	equipe INT NOT NULL,
+	joueur INT NOT NULL,
+	FOREIGN KEY (partie) REFERENCES parties (id_partie),
+	FOREIGN KEY (equipe) REFERENCES equipes (id_equipe),
+	FOREIGN KEY (joueur) REFERENCES joueurs (id_joueur),
+	PRIMARY KEY (id_inscription)
 );
 
 CREATE TABLE IF NOT EXISTS flasher (
-  date_flash date NOT NULL,
-  joueur varchar(20) NOT NULL,
-  qrcode int NOT NULL,
-  FOREIGN KEY (joueur) REFERENCES joueur(login_joueur),
-  FOREIGN KEY (qrcode) REFERENCES qrcode(id_qrcode),
-  PRIMARY KEY (date_flash,joueur,qrcode)
+	id_flash INT NOT NULL AUTO_INCREMENT,
+	date_flash DATETIME NOT NULL,
+	joueur INT NOT NULL,
+	qrcode INT NOT NULL,
+	FOREIGN KEY (joueur) REFERENCES joueurs (id_joueur),
+	FOREIGN KEY (qrcode) REFERENCES qrcodes (id_qrcode),
+	PRIMARY KEY (id_flash)
 );
 
-CREATE TABLE IF NOT EXISTS inscription (
-  id_inscription int NOT NULL,
-  date_flash date NOT NULL,
-  joueur varchar(20) NOT NULL,
-  partie int NOT NULL,
-  equipe int NOT NULL,
-  FOREIGN KEY (joueur) REFERENCES joueur(login_joueur),
-  FOREIGN KEY (partie) REFERENCES partie(id_partie),
-  FOREIGN KEY (equipe) REFERENCES equipe(id_equipe),
-  PRIMARY KEY (id_inscription)
-);
-
--- Insertions pour les tests
-
-INSERT INTO partie (date_debut, date_fin) VALUES (NOW(), NOW());
-
-INSERT INTO equipe (id_equipe) VALUES
-(1),
-(2),
-(3),
-(4);
-
-INSERT INTO joueur (login_joueur, equipe) VALUES
-('joueur1', 1),
-('joueur2', 2),
-('joueur3', 3),
-('joueur4', 4),
-('joueur5', 1);
-
-INSERT INTO zone (id_zone) VALUES
-(1),
-(2);
-
-INSERT INTO qrcode (id_qrcode, zone) VALUES
-(42, 1),
-(43, 2);
+INSERT INTO equipes (hexcolor) VALUES ('#FF0055');
+INSERT INTO equipes (hexcolor) VALUES ('#0077DD');
+INSERT INTO equipes (hexcolor) VALUES ('#00CC66');
+INSERT INTO equipes (hexcolor) VALUES ('#EEEE22');
