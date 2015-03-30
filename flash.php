7a2bf890f27e9c928a71ceb1abd890f629f85333
Thomas Riccioli <thomas.riccioli@etu.utc.fr>
2015-03-30 14:45:47 +0200

    Auto creation lorsque login n'existe pas

--- flash.php
+++ flash.php
@@ -1,8 +1,55 @@
 <?php
 	require_once('db_connect.php');
-//	echo json_encode($_POST);
+	$result = array('success' => 'NO');
 	$login = $_POST['login'];
 	$data = $_POST['data'];
+	
+	$req = $bdd->prepare('
+		SELECT *, (
+			SELECT COUNT(*)
+			FROM inscriptions
+			WHERE joueur = j.id_joueur
+		) AS num_inscriptions
+		FROM joueurs j
+		WHERE login = ?;
+	');
+	$req->execute(array($login));
+	if ($row = $req->fetch()) {
+		$nb_inscriptions = $row['num_inscriptions'];
+	} else {
+		$nb_inscriptions = 0;
+		$req = $bdd->prepare('
+			INSERT INTO joueurs (login)
+			VALUES (?);
+		');
+		$req->execute(array($login));
+	}
+	if ($nb_inscriptions == 0) {
+		try {
+			$req = $bdd->prepare('
+				INSERT INTO inscriptions (date_inscription, partie, equipe, joueur)
+				VALUES (NOW(), (
+					SELECT id_partie
+					FROM parties
+					ORDER BY date_debut DESC
+					LIMIT 1
+				), (
+					SELECT id_equipe
+					FROM equipes
+					ORDER BY RAND()
+					LIMIT 1
+				), (
+					SELECT id_joueur
+					FROM joueurs
+					WHERE login = ?
+					LIMIT 1
+				));
+			');
+			$req->execute(array($login));
+		} catch (Exception $e) {
+			$result['message'] = 'Error: Vous n\'êtes inscrit dans aucune partie';
+		}
+	}
 	try {
 		$req = $bdd->prepare('
 			INSERT INTO flashs (date_flash, joueur, qrcode)
@@ -17,8 +64,9 @@
 			$login,
 			$data
 		));
-		echo 'Success';
+		$result['success'] = 'YES';
 	} catch (Exception $e) {
-		die('Error:' . $e->getMessage());
+		$result['message'] = 'Error: ' . $e->getMessage();
+		die(json_encode($result));
 	}
 ?>
