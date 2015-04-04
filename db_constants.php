7ce2f5f7b32b204f12573e2e3e238d5fd589a67f
BaptisteAmato <baptiste.amato@gmail.com>
2015-04-04 11:34:57 +0200

    suppression tests_requete

--- db_constants.php
+++ db_constants.php
@@ -2,6 +2,6 @@
 	define('DB_HOST', 'localhost');
 	define('DB_NAME', 'si28');
 	define('DB_USER', 'root');
-	define('DB_PASSWORD', 'root');
+	define('DB_PASSWORD', '');
 	define('DB_STRUCTURE_FILE', 'db_structure.sql');
 ?>
