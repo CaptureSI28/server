77dc3b2b455f44f35be78599f4e5cfa0c2ba5320
Baptiste Amato <baptiste.amato@gmail.com>
2015-06-05 17:15:11 +0200

    Error fix

--- mobile/index.php
+++ mobile/index.php
@@ -45,7 +45,7 @@
 			// Flasher un QRCode
 			case 'flash':
 				$success = newFlash(date('Y-m-d H:i:s', time()), getIdForPlayer($_SESSION['login']), $_POST['qrcode']);
-				if ($succes == 2)
+				if ($success == 2)
 					$response['bonus'] = 'YES';
 				else
 					$response['bonus'] = 'NO';
